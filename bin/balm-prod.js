e2745a704183ae03fae97f2fe83a08026de16d10
elf-mouse <ifmouz@gmail.com>
2023-03-03 10:43:52 +0800

    feat: update balm prod

--- bin/balm-prod.js
+++ bin/balm-prod.js
@@ -1,15 +1,30 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
 import inquirer from 'inquirer';
 import setEnvironment from '../lib/env.js';
+import { getConfig } from '../lib/config.js';
 import deployProject from '../lib/deploy.js';
-
-const program = new Command();
 
 async function production() {
   await setEnvironment();
 
-  // TODO
+  const releases = getConfig('releases');
+
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'releaseBranch',
+        message: 'Please select the branch of production:',
+        choices: releases
+      },
+      {
+        type: 'confirm',
+        name: 'ok',
+        message: ({ releaseBranch }) =>
+          `Determine the release ${releaseBranch}?`
+      }
+    ])
+    .then(({ releaseBranch, ok }) => ok && deployProject(releaseBranch));
 }
 
 production();
