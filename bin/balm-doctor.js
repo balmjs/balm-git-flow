8b2f18f7822195fd49dcd34ce3bc824741196821
elf-mouse <ifmouz@gmail.com>
2023-03-06 10:58:56 +0800

    chore: update init

--- bin/balm-doctor.js
+++ bin/balm-doctor.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 import setEnvironment from '../lib/env.js';
-import { init } from '../lib/branch.js';
 
 async function doctor() {
   await setEnvironment(true);
-
-  init();
 }
 
 doctor();
