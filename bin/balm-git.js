e8a3207e391096b7f89d49506574bf566a95a562
elf-mouse <ifmouz@gmail.com>
2023-03-07 13:09:08 +0800

    0.3.0

--- bin/balm-git.js
+++ bin/balm-git.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
+import { createRequire } from 'node:module';
 import { Command } from 'commander';
 
+const requireModule = createRequire(import.meta.url);
+const pkg = requireModule('../package.json');
 const program = new Command();
 
+const version = `balm-git-flow: ${pkg.version}`;
+
 program
+  .version(version)
   .usage('<command> [options]')
   .command('doctor', 'check the project environment')
   .command('dev', 'create new branch for development from origin main branch')
