2d885c51dc9cbbb603c9a7d8e826daeac55ae82a
elf-mouse <ifmouz@gmail.com>
2023-03-08 11:56:00 +0800

    chore: update

--- bin/balm-prod.js
+++ bin/balm-prod.js
@@ -10,65 +10,60 @@ async function production() {
   await setEnvironment();
 
   const { main, release, releases, scripts } = getConfig();
+  const currentBranch = await getCurrentBranch();
+  const devBranches = await getDevelopmentBranches();
 
-  if (scripts.length) {
-    const currentBranch = await getCurrentBranch();
-    const devBranches = await getDevelopmentBranches();
-
-    inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'releaseBranch',
-          message: 'Please select the production branch:',
-          choices: releases
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'releaseBranch',
+        message: 'Please select the production branch:',
+        choices: releases
+      },
+      {
+        type: 'list',
+        name: 'devBranch',
+        message: 'Please select the development branch to be merged:',
+        choices: devBranches,
+        when: ({ releaseBranch }) =>
+          currentBranch === main && releaseBranch === release
+      },
+      {
+        type: 'list',
+        name: 'releaseScript',
+        message: 'Please select the command of npm-run-script:',
+        default: ({ releaseBranch }) => {
+          const index = releases.indexOf(releaseBranch);
+          return scripts[index];
         },
-        {
-          type: 'list',
-          name: 'devBranch',
-          message: 'Please select the development branch to be merged:',
-          choices: devBranches,
-          when: ({ releaseBranch }) =>
-            currentBranch === main && releaseBranch === release
-        },
-        {
-          type: 'list',
-          name: 'releaseScript',
-          message: 'Please select the command of npm-run-script:',
-          default: ({ releaseBranch }) => {
-            const index = releases.indexOf(releaseBranch);
-            return scripts[index];
-          },
-          choices: scripts
-        },
-        {
-          type: 'confirm',
-          name: 'ok',
-          message: ({ releaseBranch, releaseScript }) => {
-            let msg = `Determine the release '${releaseBranch}' branch`;
-            if (releaseScript) {
-              msg += ` using the '${releaseScript}' command`;
-            }
-            return `${msg}?`;
+        choices: scripts
+      },
+      {
+        type: 'confirm',
+        name: 'ok',
+        message: ({ releaseBranch, releaseScript }) => {
+          let msg = `Determine the release '${releaseBranch}' branch`;
+          if (releaseScript) {
+            msg += ` using the '${releaseScript}' command`;
           }
-        },
-        {
-          type: 'input',
-          name: 'logMessage',
-          message: 'Please input the release log message:'
+          return `${msg}?`;
         }
-      ])
-      .then(
-        (answers) =>
-          answers.ok &&
-          deployProject({
-            currentBranch,
-            ...answers
-          })
-      );
-  } else {
-    logger.fatal(`Missing 'BALM_GIT_FLOW_SCRIPTS' in balm.env.js`);
-  }
+      },
+      {
+        type: 'input',
+        name: 'logMessage',
+        message: 'Please input the release log message:'
+      }
+    ])
+    .then(
+      (answers) =>
+        answers.ok &&
+        deployProject({
+          currentBranch,
+          ...answers
+        })
+    );
 }
 
 production();
