42d5633652a328f25189a812ffcee0f7246a5114
elf-mouse <ifmouz@gmail.com>
2023-03-17 17:27:27 +0800

    test: update

---
+++ test/balm.env.js
@@ -0,0 +1,6 @@
+process.env.BALM_GIT_FLOW_DEBUG = true;
+process.env.BALM_GIT_FLOW_MAIN = 'main';
+process.env.BALM_GIT_FLOW_RELEASE = 'release';
+process.env.BALM_GIT_FLOW_RELEASES = ['test', 'release'];
+process.env.BALM_GIT_FLOW_SCRIPTS = ['prod'];
+process.env.BALM_GIT_FLOW_BUILD_DIR = 'dist';
