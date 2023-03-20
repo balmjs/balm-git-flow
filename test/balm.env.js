b7540fd197cc386aa55b8fc106f1616f4ae760d7
elf-mouse <ifmouz@gmail.com>
2023-03-20 09:25:58 +0800

    chore: update

--- test/balm.env.js
+++ test/balm.env.js
@@ -1,6 +1,10 @@
-process.env.BALM_GIT_FLOW_DEBUG = true;
+// process.env.BALM_GIT_FLOW_DEBUG = true;
 process.env.BALM_GIT_FLOW_MAIN = 'main';
 process.env.BALM_GIT_FLOW_RELEASE = 'release';
 process.env.BALM_GIT_FLOW_RELEASES = ['test', 'release'];
 process.env.BALM_GIT_FLOW_SCRIPTS = ['prod'];
 process.env.BALM_GIT_FLOW_BUILD_DIR = 'assets';
+process.env.BALM_GIT_FLOW_REPOSITORIES = [
+  'git@github.com:balmjs/balm-git-flow-test.git'
+];
+// process.env.BALM_GIT_FLOW_SITE = 'gh-pages';
