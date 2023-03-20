e118efab66b346e8b3afbcc9f1b41487c10a5c9b
elf-mouse <ifmouz@gmail.com>
2023-03-20 09:55:34 +0800

    test: update

--- test/balm.env.js
+++ test/balm.env.js
@@ -3,7 +3,7 @@ process.env.BALM_GIT_FLOW_MAIN = 'legacy';
 process.env.BALM_GIT_FLOW_RELEASE = 'release';
 process.env.BALM_GIT_FLOW_RELEASES = ['test', 'release'];
 process.env.BALM_GIT_FLOW_SCRIPTS = ['prod'];
-process.env.BALM_GIT_FLOW_BUILD_DIR = 'dist';
+process.env.BALM_GIT_FLOW_BUILD_DIR = 'assets';
 process.env.BALM_GIT_FLOW_REPOSITORIES = [
   'git@github.com:balmjs/balm-git-flow-test.git'
 ];
